09c9e80a5cf338cf1872f9c83268e386745fbd36
amit <rishi.mishra@skilrock.com>
2017-03-31 16:18:23 +0530

    multiple

--- src/Operation.java
+++ src/Operation.java
@@ -5,9 +5,14 @@ public class Operation {
 		System.out.println(a + b);
 	}
 	
+	public void multi(int a, int b){
+		System.out.println(a * b);
+	}
+	
 	public static void main(String...arg){
 		Operation oprObj = new Operation();
 		oprObj.sum(3, 5);
+		oprObj.multi(3, 5);
 	}
 
 }
